7f56c30869840f3b9c451e8b5feef2228872c428
Lovin Sharma <130526452+Shawarm0@users.noreply.github.com>
2026-09-03 17:03:45 +0100

    format change

--- sdl.c
+++ sdl.c
@@ -20,14 +20,17 @@ void handle_input(chip8_t *chip8) {
   SDL_Event event;
 
   while (SDL_PollEvent(&event)) {
+
+    // TODO: HANDLE OTHER KEY INPUTS
+
     switch (event.type) {
 
     case SDL_QUIT:
-      // TODO: Handle other key inputs.
       chip8->state = QUIT;
       return;
 
     case SDL_KEYDOWN:
+
       switch (event.key.keysym.sym) {
       case SDLK_ESCAPE:
         chip8->state = QUIT;
@@ -35,6 +38,7 @@ void handle_input(chip8_t *chip8) {
       default:
         break;
       }
+
       break;
 
     case SDL_KEYUP:
