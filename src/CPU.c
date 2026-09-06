8d38720aaae803fdfa2f03c5a4b8851071774f2f
Lovin Sharma <130526452+Shawarm0@users.noreply.github.com>
2026-09-06 20:08:33 +0100

    Implement 0x2NNN call subroutine

--- src/CPU.c
+++ src/CPU.c
@@ -92,5 +92,11 @@ void emulate_instruction(chip8_t *chip8) {
     chip8->PC = chip8->inst.NNN;
     break;
 
+  case 0x2000:
+    chip8->SP += 1;
+    chip8->stack[chip8->SP] = chip8->PC;
+    chip8->PC = chip8->inst.NNN;
+    break;
+
   }
 }
