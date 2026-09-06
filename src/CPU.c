7169a69d4a8bebbb72389e5a3717f5c5e3560769
Lovin Sharma <130526452+Shawarm0@users.noreply.github.com>
2026-09-06 20:08:33 +0100

    Implement 0x5XY0 skip if VX equals VY

--- src/CPU.c
+++ src/CPU.c
@@ -113,5 +113,12 @@ void emulate_instruction(chip8_t *chip8) {
     };
 
     break;
+  case 0x5000:
+
+    if (chip8->V[chip8->inst.X] == chip8->V[chip8->inst.Y]) {
+      chip8->PC += 2;
+    };
+    break;
+
   }
 }
