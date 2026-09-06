aca1479b01a8d2effe4be4cdc3753f5a42ee5eb2
Lovin Sharma <130526452+Shawarm0@users.noreply.github.com>
2026-09-06 22:09:21 +0100

    Implement 0x8XYE shift VX left

--- src/CPU.c
+++ src/CPU.c
@@ -170,8 +170,12 @@ void emulate_instruction(chip8_t *chip8) {
       chip8->V[0xF] = no_borrow;
       break;
     }
-    case 0xE:
+    case 0xE: {
+      uint8_t msb = chip8->V[chip8->inst.X] & 0xF0;
+      chip8->V[chip8->inst.X] <<= 1;
+      chip8->V[0xF] = msb;
       break;
+    }
     }
 
     break;
